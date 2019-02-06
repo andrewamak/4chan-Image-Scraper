6fcb0a7bbf4718709c289e57e4c1b68bd4d9fd4f
andrewamak <41027149+andrewamak@users.noreply.github.com>
2019-02-06 06:57:58 -0800

    Update 4chanScraper.py
    changed directory, added a folder naming option

--- 4chanScraper.py
+++ 4chanScraper.py
@@ -6,18 +6,24 @@ import os
 # example url: url = "https://boards.4channel.org/w/thread/2113466"
 #put in a 4chan thread url for bs to read
 url = input('Please enter a 4chan URL: ')
+folder_name = input('Enter folder name(Press ENTER for default): ') 
 req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 page = urllib.request.urlopen(req).read()
 soup = bs.BeautifulSoup(page,'lxml')
 
 #creating a path onto the desktop named with the thread ID number
-my_path = '/home/andy/Desktop/' + url[-7:]
+if folder_name == '':
+    my_path = '/home/andy/Pictures/' + url[-7:]
+else:
+    my_path = '/home/andy/Pictures/' + folder_name
+                    
 os.makedirs(my_path)
 
 #creating an empty list for the src to fill up
 image_box = []
 
 #acquiring the images with their sources and appending them into the 'image_box'
+print('Downloading pictures...')
 for links in soup.find_all('img'):
     image_source = links.get("src")
     image_source = 'https:' + image_source[:-5]
